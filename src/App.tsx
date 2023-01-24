b6a2f51fa621e3a8fb027f48b4f1270920913334
João Marcelino <joaoluizmarcelinoribeiro@gmail.com>
2023-01-23 23:31:02 -0300

    Adding a Static Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BringName from './components/Name/BringName';
+
+import './styles/App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	return (
+		<div className="App">
+
+			<header>
+
+				<h2>Amigo Secreto</h2>
+
+				<input
+					type="text" autoComplete="off" 
+					placeholder="Escrever task..." 
+					name="task"
+					className="input"
+				/>
+
+				<button type="submit" className="btn-header">Adicionar Nome</button>
+			</header>
+			
+			<div className="line"></div>
+
+			<BringName />
+			
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Name/BringName.tsx
@@ -0,0 +1,18 @@
+import './styles.css'
+
+function BringName() {
+	
+	return (
+		<div className="card">
+			<div>
+                <p>João</p>
+            </div>
+
+            <div className="line2" >
+            <span className="btn-card">X</span>
+            </div>
+		</div>
+	);
+}
+
+export default BringName;
